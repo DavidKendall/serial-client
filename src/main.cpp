2e772e47833998bb7c4db4520efedeffa65faa3b
David Kendall <david.kendall@northumbria.ac.uk>
2019-01-23 19:16:27 +0000

    Now ok to start client before server

--- src/main.cpp
+++ src/main.cpp
@@ -2,26 +2,33 @@
 #include <stdbool.h>
 #include <assert.h>
 
-Serial client(D1, D0, 115200);
+RawSerial client(D1, D0, 115200);
 Serial pc(USBTX, USBRX, 115200);
 
 char clientBuffer[128];
 
 int main() {
+  int c;
   uint32_t i;
 
   pc.printf("Serial Client\n");
 
   while (true) {
-    if (client.putc('\xA5') == '\xA5') {
-      if (client.putc('\x50') == '\x50') {
+    while (! client.readable()) {
+      while (! client.writeable()) {
       }
-      for (i=0; i<20; i++) {
-        clientBuffer[i] = client.getc();
+      c = client.putc('\xA5');
+      assert(c == '\xA5');
+      while (! client.writeable()) {
       }
-      clientBuffer[20] = '\n';
-      pc.printf("%s", clientBuffer);
+      c = client.putc('\x50');
+      assert(c == '\x50');
     }
+    for (i=0; i<20; i++) {
+      clientBuffer[i] = client.getc();
+    }
+    clientBuffer[20] = '\n';
+    pc.printf("%s", clientBuffer);
     wait_ms(2);
   }
 }
