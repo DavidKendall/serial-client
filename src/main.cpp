532ff1933a8e83e06a3d6195140e879d3f02aa0b
David Kendall <david.kendall@northumbria.ac.uk>
2019-01-24 10:24:45 +0000

    Collect stats at client

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <mbed.h>
 #include <stdbool.h>
 #include <assert.h>
+#include "counter.h"
 
 UARTSerial client(D1, D0, 115200);
 Serial pc(USBTX, USBRX, 115200);
@@ -11,7 +12,14 @@ char clientBuffer[128];
 int main() {
   ssize_t nBytes;
   int nReceived;
+  uint32_t nIter = 0;
+  uint64_t total = 0;
+  uint32_t t;
+  uint32_t min = (1ULL << 32) - 1;
+  uint32_t mean = 0;
+  uint32_t max = 0;
 
+  counterInit();
   pc.printf("Serial Client\n");
 
   while (true) {
@@ -21,15 +29,26 @@ int main() {
       nBytes = client.write(command, sizeof(command));
       assert(nBytes == sizeof(command));
     }
-
+    
+    counterStart();
     nReceived = 0;
     while (nReceived < 20) {
       nBytes = client.read(&clientBuffer[nReceived], 20 - nReceived);
       nReceived += nBytes;
     }
+    t = counterStop();
     
-    clientBuffer[20] = '\n';
-    pc.printf("%s", clientBuffer);
+    nIter += 1;
+    assert(nIter <= (1ULL << 32) - 1);
+    total += t;
+    mean = total / nIter;
+    if (t < min) {
+      min = t;
+    }
+    if (t > max) {
+      max = t;
+    }
+    pc.printf("%s %ld %ld % ld %ld\n", clientBuffer, t, min, mean, max);
     wait_ms(2);
   }
 }
