e01279cc852dad5a04b80ff0a191ffbe1496e1c1
David Kendall <david.kendall@northumbria.ac.uk>
2019-01-24 10:55:44 +0000

    Collect client stats

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <mbed.h>
 #include <stdbool.h>
 #include <assert.h>
+#include "counter.h"
 
 RawSerial client(D1, D0, 115200);
 Serial pc(USBTX, USBRX, 115200);
@@ -10,7 +11,14 @@ char clientBuffer[128];
 int main() {
   int c;
   uint32_t i;
+  uint32_t nIter = 0;
+  uint64_t total = 0;
+  uint32_t t;
+  uint32_t min = (1ULL << 32) - 1;
+  uint32_t mean = 0;
+  uint32_t max = 0;
 
+  counterInit();
   pc.printf("Serial Client\n");
 
   while (true) {
@@ -24,11 +32,23 @@ int main() {
       c = client.putc('\x50');
       assert(c == '\x50');
     }
+    counterStart();
     for (i=0; i<20; i++) {
       clientBuffer[i] = client.getc();
     }
-    clientBuffer[20] = '\n';
-    pc.printf("%s", clientBuffer);
+    t = counterStop();
+
+    nIter += 1;
+    assert(nIter <= (1ULL << 32) - 1);
+    total += t;
+    mean = total / nIter;
+    if (t < min) {
+      min = t;
+    }
+    if (t > max) {
+      max = t;
+    }
+    pc.printf("%s %ld %ld % ld %ld\n", clientBuffer, t, min, mean, max);
     wait_ms(2);
   }
 }
